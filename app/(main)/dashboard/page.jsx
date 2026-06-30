29d39a941114612068ae117c3df0ab5e043b6157
aakashhh-17 <aakashbhujbal1704@gmail.com>
2026-06-30 11:22:14 +0530

    worked on budget functionality

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,32 +3,52 @@ import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/budget-progress";
 
-async function DashboardPage(){
-    const accounts = await getUserAccounts()
+async function DashboardPage() {
+  const accounts = await getUserAccounts();
 
-    return <div className="px-5">
-        {/* Budget progress */}
+  const defaultAccount = accounts?.find((account) => account.isDefault);
 
-        {/* Overview */}
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
-        {/* Accounts grid */}
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+  return (
+    <div className="space-y-8">
+      {/* Budget progress */}
+      { defaultAccount && 
+      <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
+      }
+      {/* Overview */}
+
+      {/* Accounts grid */}
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
-            <Card className={'hover:shadow-md transition-shadow cursor-pointer border-dashed'}>
-                <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
-                    <Plus className="h-10 w-10 mb-2" />
-                    <p className="text-sm font-medium">Add new account</p>
-                </CardContent>
-            </Card>
+          <Card
+            className={
+              "hover:shadow-md transition-shadow cursor-pointer border-dashed"
+            }
+          >
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add new account</p>
+            </CardContent>
+          </Card>
         </CreateAccountDrawer>
 
-
-        {accounts.length > 0 && accounts.map((account)=>{
-            return <AccountCard key={account.id} account={account} />
-        })}
-        </div>
+        {accounts.length > 0 &&
+          accounts.map((account) => {
+            return <AccountCard key={account.id} account={account} />;
+          })}
+      </div>
     </div>
+  );
 }
 
 export default DashboardPage;
--- components/ui/progress.jsx
+++ components/ui/progress.jsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils"
 function Progress({
   className,
   value,
+  extraStyles,
   ...props
 }) {
   return (
@@ -20,7 +21,7 @@ function Progress({
       {...props}>
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="size-full flex-1 bg-primary transition-all"
+        className={`size-full flex-1 bg-primary transition-all ${extraStyles}`}
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
     </ProgressPrimitive.Root>
   );
